7d79ee464bc71881129746db5b0ebc041944cf35
O10 <aleksander.k.wojcik@gmail.com>
2014-04-30 15:05:49 +0200

    chromosom - funckja licząca fitness
    Pierwsza wersja powinna działać dla indeksowania prac oraz maszyn od 0,
    nietestowana

--- Chromosom.cpp
+++ Chromosom.cpp
@@ -39,24 +39,34 @@ void Chromosom::generateRandomGenotype() {
 	// TODO Cialo funkcji
 }
 
-int Chromosom::countFitness() {
-    vector<int> machineSchedule;	//!< Informacje o zajetosci maszyn.
-	vector<int> jobSchedule;	//!< Informacje o postepach prac.
+int Chromosom::countFitness() { //dziaÅ‚a przy numeracji maszyn i jobÃ³w od 0 - do ustalenia
+    vector<int> machineSchedule(machineCount,0);	//!< Informacje o zajetosci maszyn.
+	vector<int> jobSchedule(jobCount,0);	//!< Informacje o postepach prac.
 	vector<int> currentTaskCount(jobCount,0);
 	int startingfit=0;
-	int currentTask;
+	int currentTasknum;
+	int currentMachineId;
 	for(vector<int>::iterator it=genotype.begin();it!=genotype.end();it++)
 	{
-	    currentTask=currentTaskCount[*it];
+	    currentTasknum=currentTaskCount[*it];
+	    currentMachineId=jobDatabase.getJobs()[*it].getTaskList()[currentTasknum].getMachine()->getId();
 	    currentTaskCount[*it]++;
-	    if(machineSchedule[*it]>jobSchedule[*it])
-	    {
-	        jobSchedule[*it]=machineSchedule[*it]+jobDatabase.getJobs[*it].getTaskList[currentTask].getTime();
-	    }
 
+	    if(machineSchedule[currentMachineId]>jobSchedule[*it])
+	        jobSchedule[*it]=machineSchedule[currentMachineId]+jobDatabase.getJobs()[*it].getTaskList()[currentTasknum].getTime();
+	    else
+	        jobSchedule[*it]+=jobDatabase.getJobs()[*it].getTaskList()[currentTasknum].getTime();
+
+	    machineSchedule[currentMachineId]+=jobDatabase.getJobs()[*it].getTaskList()[currentTasknum].getTime();
 	}
+	for(vector<int>::iterator it=jobSchedule.begin();it!=jobSchedule.end();it++)
+	    if(jobSchedule[*it]>startingfit)
+            startingfit=jobSchedule[*it];
+
+    this->fitness=startingfit;
+    return this->fitness;
 }
 
 int Chromosom::jobCount=0;	//!< Liczba zadan do wykonania.
 int Chromosom::machineCount=0;
-Database Chromosom::jobDatabase;  //Brak kompilacji dopóki nie bêdzie definicji konstruktora
+Database Chromosom::jobDatabase;
