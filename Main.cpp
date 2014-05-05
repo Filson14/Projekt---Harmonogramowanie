9f1f40530b864737c6030384ff27f0abc48c643b
Filson14 <fpasternak14@gmail.com>
2014-05-05 22:08:34 +0200

    zmiany kosmetyczne
    poprawiony plik test5, sprawdzenie powodzenia odczytania pliku,
    prezentacja danych przed i po wykonaniu algorytmu

--- Main.cpp
+++ Main.cpp
@@ -9,16 +9,20 @@
 int main(int argc, char *argv[]){
 	srand(time(NULL));
 
-	Chromosom::getJobDatabase().readFromFile("/Users/Wojtek/Documents/Eclipse/Harmonogramowanie/Projekt-Harmonogramowanie/test3.txt");
-	Chromosom::setJobCount(Chromosom::getJobDatabase().getJobsAmount());
-	Chromosom::setMachineCount(Chromosom::getJobDatabase().getMachinesAmount());
+	if(Chromosom::getJobDatabase().readFromFile("test5.txt")){
+		Chromosom::setJobCount(Chromosom::getJobDatabase().getJobsAmount());
+		Chromosom::setMachineCount(Chromosom::getJobDatabase().getMachinesAmount());
 
-	SettingsProblem problem = {Chromosom::getJobDatabase().getJobsAmount(), Chromosom::getJobDatabase().getMachinesAmount()};
-	SettingsAlgorithm algorithm = {30, 15, 16, 20, 0.75, 0.35};
-	SettingsOperator operators = {new SelectionTournament(0.5, 8), new MutationInversion(), new CrossoverTwoPoint()};
+		Chromosom::getJobDatabase().presentData();
 
-	Algorithm algo(problem, algorithm, operators);
-	algo.runAlgorithm();
+		SettingsProblem problem = {Chromosom::getJobDatabase().getJobsAmount(), Chromosom::getJobDatabase().getMachinesAmount()};
+		SettingsAlgorithm algorithm = {30, 15, 16, 20, 0.75, 0.35};
+		SettingsOperator operators = {new SelectionTournament(0.5, 8), new MutationInversion(), new CrossoverTwoPoint()};
 
+		Algorithm algo(problem, algorithm, operators);
+		algo.runAlgorithm();
+
+		Chromosom::getJobDatabase().presentData();
+	}
 	return 0;
 }
