9010704969b678fbf27749f993749e3a5cf824d8
Filson14 <fpasternak14@gmail.com>
2014-05-05 13:28:29 +0200

    main

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,11 @@
+#include "Algorithm.h"
 #include <iostream>
 #include "./DataModification/Database.h"
 #include "./DataModification/Job.h"
 #include "./DataModification/Machine.h"
 #include "./DataModification/Task.h"
-#include "Algorithm.h"
+#include "SettingsStructures.h"
+
 int main(int argc, char *argv[]){
 	srand(time(NULL));
 
@@ -11,8 +13,12 @@ int main(int argc, char *argv[]){
 	Chromosom::setJobCount(Chromosom::getJobDatabase().getJobsAmount());
 	Chromosom::setMachineCount(Chromosom::getJobDatabase().getMachinesAmount());
 
+	SettingsProblem problem = {Chromosom::getJobDatabase().getJobsAmount(), Chromosom::getJobDatabase().getMachinesAmount()};
+	SettingsAlgorithm algorithm = {5, 5, 20, 60, 0.5, 0.5};
+	SettingsOperator operators = {new SelectionTournament(0.95, 10), new MutationInversion(), new CrossoverTwoPoint()};
 
-
+	Algorithm algo(problem, algorithm, operators);
+	algo.runAlgorithm();
 
 
 	return 0;
