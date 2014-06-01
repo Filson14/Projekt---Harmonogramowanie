f0c06d4c98aec3291714a8575508fc9e384322bb
O10 <aleksander.k.wojcik@gmail.com>
2014-06-01 10:55:27 +0200

    commenty

--- GUI/blockplot.h
+++ GUI/blockplot.h
@@ -14,8 +14,12 @@ class BlockPlot : public QCustomPlot
 {
     Q_OBJECT
 private:
-    QVector<QColor> cVect;
+    QVector<QColor> cVect; //!< Wektor przechowujący kolory bloczkow podczas rysowania wykresu
 public:
+    /**
+     * Standardowy konstruktor każdej klasy dziedziczącej po QWidget
+     * \param parent Rodzic QWidgetu.
+     */
     explicit BlockPlot(QWidget *parent = 0);
     virtual ~BlockPlot();
 
@@ -23,7 +27,13 @@ signals:
 
 
 public slots:
+    /**
+     * Slot bedący odpowiedzialny za ponowne rysowanie wykresu w przypadku zmiany danych.
+     */
     void onDataChanged(Database* );
+    /**
+     * Slot odpowiedzialny za wyświetlanie informacji o konkretnym bloczku w postaci Tooltip-u.
+     */
     void onBlockSelected(bool);
 
 
--- GUI/extqcpstatisticalbox.h
+++ GUI/extqcpstatisticalbox.h
@@ -10,9 +10,17 @@ class ExtQCPStatisticalBox : public QCPStatisticalBox
     Q_OBJECT
 
 public:
+    /**
+     * Standardowy konstruktor klasy dziedziczącej po QCPStatisticalBox.
+     * \param keyAxis Oś zawierająca dziedzinę funkcji.
+     * \param valueAxis Oś zawierająca wartości funkcji.
+     */
     ExtQCPStatisticalBox (QCPAxis *keyAxis, QCPAxis *valueAxis);
 
 public slots:
+    /**
+     * Slot odpowiedzialny za wyświetlanie informacji o konkretnym bloczku w postaci QMessageBoxa.
+     */
     void blockinfo(bool);
 
 };
